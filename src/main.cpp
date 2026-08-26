93a55dc3b37470748d06f564dc56a1ab4dfa5624
Minh Nguyen <minh0910200444@gmail.com>
2026-08-26 15:07:21 +0700

    update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#define LED_PIN 13
+#define SENSOR_PIN A0
+
+int threshold = 500;
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(SENSOR_PIN, INPUT);
+}
+
+void loop() {
+    int value = analogRead(SENSOR_PIN);
+    if (value > threshold) {
+        digitalWrite(LED_PIN, HIGH);
+    } else {
+        digitalWrite(LED_PIN, LOW);
+    }
+}
